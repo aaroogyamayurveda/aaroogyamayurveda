fd555a5f0ddd3635b1ac8005c71f2c5c7788d1b6
aaroogyamayurveda <aaroogyamayurveda@gmail.com>
2026-08-23 01:07:18 +0530

    Refresh CRM2 lead context bridge loader

--- crm1/advanced-business-layer.js
+++ crm1/advanced-business-layer.js
@@ -21,5 +21,5 @@
   await load('./crm1-lead-call-bridge.js');
   await load('./crm1-lead-workqueue.js');
   await load('./crm1-telephony-bridge-readiness.js');
-  await load('./crm1-lead-context-bridge.js');
+  await load('./crm1-lead-context-bridge.js?v=2');
 })();
