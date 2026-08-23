4f064877b1da8e1fe2be5447692c649b649547f9
aaroogyamayurveda <aaroogyamayurveda@gmail.com>
2026-08-23 14:06:54 +0530

    Load Customer360 timeline cleanup module

--- crm1/advanced-business-layer.js
+++ crm1/advanced-business-layer.js
@@ -30,4 +30,5 @@
   await load('./crm1-manager-reports.js?v=1');
   await load('./crm1-order-timeline.js?v=4');
   await load('./crm1-delivery-workflow.js?v=1');
+  await load('./crm1-customer360-timeline-cleanup.js?v=1');
 })();
